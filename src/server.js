3ef28fffeeb17e65d53c9eda03fe5bbf93703ac0
kanua <kanua0719@gmail.com>
2020-11-01 20:24:51 +0900

    make server.js

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(express.static('./'));
+app.use(express.static(path.join('./', 'dist')));
+app.get('/*', function(req, res){
+	res.sendFile(path.join('./', 'dist', 'index.html'));
+});
+
+app.listen(port);
