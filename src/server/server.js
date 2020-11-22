2f8cf6fb24ecf1ce9feb2ac80c09d6be8ec152f5
kanua <kanua0719@gmail.com>
2020-11-22 17:19:23 +0900

    use dev-server then at local.

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+//const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 const PATH_CLIENT = path.resolve(__dirname, '..', 'client');
 const getPostgresClient = require('./postgres').getPostgresClient;
@@ -8,26 +9,6 @@ const getPostgresClient = require('./postgres').getPostgresClient;
 app.use(express.static('./'));
 app.use(express.static(path.join('./', 'dist')));
 app.use(express.static(path.join('./', 'dist','client')));
-
-console.log(`server is running at ${port}`);
-//console.log(`dirname is ${__dirname}`);
-
-app.get('/api/read/', function(req, res){
-	console.log('DB: Read');
-	(async() => {
-		try {
-			const db = await getPostgresClient();
-			const SQL = 'SELECT * FROM hoge';
-			const result = await db.execute(SQL);
-			//console.log(result);
-			res.status(200).send(result);
-		} catch (e) {
-			console.log('#ERROR#: unknown');
-			res.status(500).send("DB Error.");
-			throw e;
-		}
-	})();
-});
 
 app.get('/api/write/:name', function(req, res){
 	//console.log('DB: write: ' + req.params.name);
@@ -37,7 +18,7 @@ app.get('/api/write/:name', function(req, res){
 			const SQL = "INSERT INTO hoge(foo) VALUES ($1)";
 			const result = await db.execute(SQL, [req.params.name]);
 			//console.log(result);
-			res.status(200).send("Succeed!");
+			res.status(200).send("Write: Succeed!");
 		} catch (e) {
 			console.log('#ERROR#: unknown');
 			res.status(500).send("DB Error.");
@@ -52,7 +33,24 @@ app.get('/api/delete/', function(req, res){
 			const db = await getPostgresClient();
 			const SQL = "DELETE from hoge";
 			const result = await db.execute(SQL);
-			res.status(200).send("Succeed!");
+			res.status(200).send("Delete: Succeed!");
+		} catch (e) {
+			console.log('#ERROR#: unknown');
+			res.status(500).send("DB Error.");
+			throw e;
+		}
+	})();
+});
+
+app.get('/api/read/', function(req, res){
+	console.log('DB: Read');
+	(async() => {
+		try {
+			const db = await getPostgresClient();
+			const SQL = 'SELECT * FROM hoge';
+			const result = await db.execute(SQL);
+			//console.log(result);
+			res.status(200).send(result);
 		} catch (e) {
 			console.log('#ERROR#: unknown');
 			res.status(500).send("DB Error.");
@@ -81,4 +79,6 @@ app.get('/*', function(req, res){
 	res.sendFile(path.resolve(PATH_CLIENT, 'index.html'));
 });
 
-app.listen(port);
+app.listen(port, () => {
+	console.log("server is running at "+port);
+});
